508dcd1c66f1c5636d8629cf1a821de7e3500005
NolanWeemaels <Nolan.weemaels@student.ehb.be>
2024-12-22 17:13:44 +0100

    tried fixing it again :)

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+
+function updateVh() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  
+  // Voer de functie uit bij laden en aanpassen van het scherm
+  updateVh();
+  window.addEventListener('resize', updateVh);
